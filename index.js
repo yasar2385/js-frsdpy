8e1ba2b7e3d46f489b660c10faff8749ed3aea5b
yasar2385 <yasar.mohideen@newgen.co>
2022-07-29 11:00:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ var Duplicate_Array = function (_Array, _Array2, Option) {
       return uniqueArray;
     } else {
       console.log('e');
-      Option = Option ? Option : { remove: !0, find: !1 };
+      Option = Option ? Option : { remove: !0, find: !1,bool:!1 };
       var commonArray = _Array.filter(function (item) {
         if (Option.remove) return _Array2.indexOf(item) == -1;
         else if (Option.find) return _Array2.indexOf(item) != -1;
       });
-      return commonArray;
+      return Option.bool?commonArray.length>0:commonArray;
     }
   } catch (err) {
     console.warn(err.message);
@@ -42,4 +42,4 @@ var arr = ['check', 'demo'];
 var arr_2 = ['check'];
 var arr_3 = ['demo'];
 console.log(Duplicate_Array(arr, arr_2, { find: !0 }));
-console.log(Duplicate_Array(arr, arr_3, { find: !0 }));
+console.log(Duplicate_Array(arr, arr_3, { find: !0,bool: !0 }));
